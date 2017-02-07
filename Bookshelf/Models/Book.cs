3bd90191e5f5407226ac089f5e89f00249b69a94
Jamie Burke <jamieburke1020@gmail.com>
2017-02-07 13:51:21 -0500

    first commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]           //this is the data annotation and need to add the using.Statement above (only one Key per record)
+        public int ID { get; set; } //unique identification for a database
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }   
+
+        [ForeignKey("Category")]  //Primary key in another table this is the format.  This is the data annotation and the using.Statement above with "Schema" is used
+        public int CategoryID {get; set;}
+        public virtual Category Category { get; set; }
+
+    }
+
+    
+}
